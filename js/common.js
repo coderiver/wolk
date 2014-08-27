49d47e32b604f0aacfdaeb89a0396fc3a9b0a0f4
akella <akella.a@gmail.com>
2014-08-27 16:36:01 +0300

    go

--- js/common.js
+++ js/common.js
@@ -3,6 +3,8 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-
+	$('.country').click(function(event) {
+		$(this).toggleClass('is-active');
+	});
 	console.log($('body').html());
 });
