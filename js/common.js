36f0a74207ec1530de735f9d9688dee04f2609f4
Valera19900 <kaktuz34@gmail.com>
2014-09-05 11:20:57 +0300

    fancybox

--- js/common.js
+++ js/common.js
@@ -9,4 +9,16 @@ head.ready(function() {
 	$('.select select').change(function(event) {
 		$(this).prev().text($(this).val());
 	});
+
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	$(".js-open-place").click(function(){
+		$(".js-place").removeClass("is-active")
+		$(this).parent().addClass("is-active");
+	});
 });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/jquery.fancybox.js",
 					"js/common.js");
